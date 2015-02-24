51009c8abca22f46af6e2968f3e287747156d115
Songmu <y.songmu@gmail.com>
2015-02-24 21:13:16 +0900

    verify x-hub-signature when secret is specified

--- lib/octoks/receiver.rb
+++ lib/octoks/receiver.rb
@@ -1,12 +1,16 @@
 require 'rack'
 require 'json'
+require 'openssl'
+require 'secure_compare'
 
 module Octoks
   class Receiver
     attr_accessor :hooks
+    attr_reader   :secret
 
-    def initialize
-      @hooks = {}
+    def initialize(secret = nil)
+      @hooks  = {}
+      @secret = secret
     end
 
     def on(name, &cb)
@@ -29,6 +33,10 @@ module Octoks
         return failed
       end
 
+      unless verify_signature(req)
+        return failed
+      end
+
       begin
         payload = JSON.parse(req.params['payload'])
       rescue
@@ -42,5 +50,15 @@ module Octoks
 
       [200, [], ["OK"]]
     end
+
+    HMAC_DIGEST = OpenSSL::Digest.new('sha1')
+    def verify_signature(req)
+      return true unless @secret
+      return false unless req.body
+      sig = 'sha1='+OpenSSL::HMAC.hexdigest(HMAC_DIGEST, @secret, req.body.read)
+      req.body.rewind
+      SecureCompare.compare(sig, req.env["HTTP_HUB_SIGNATURE"])
+    end
+
   end
 end
